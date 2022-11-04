3ef84cdccb895ac59955c1641dd39d570f4569b6
Jhonatan Wendel <wendeljhonatan@gmail.com>
2022-11-04 20:46:50 -0300

    folder name correction | sales by match [JS]

---
+++ HackerRank/sales by match/solution.js
@@ -0,0 +1,15 @@
+function sockMerchant(n, ar) {
+    let pair = 0
+
+    for(let i = 0; i < n; i++) {
+        for(let j = i + 1; j < n; j++) {
+            if(ar[i] === ar[j] && ar[i] !== null) {
+                pair++
+                ar[j] = null
+                break
+            }
+        }
+    }
+    
+    return pair
+}
