bc00d8ad3b9fa6b9e499bc67d42b3e5e6ebba7ad
Jhonatan Wendel <wendeljhonatan@gmail.com>
2022-11-09 04:05:29 -0300

    cats and a mouse [PY]

---
+++ HackerRank/cats and a mouse/solution.py
@@ -0,0 +1,7 @@
+def catAndMouse(x, y, z):
+    if(abs(z - y) > abs(z - x)):
+        return 'Cat A'
+    elif(abs(z - x) > abs(z - y)):
+        return 'Cat B'        
+    else:
+        return 'Mouse C'
