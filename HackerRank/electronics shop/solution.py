8bf11a6b58043f5bf7e5169346e5baac9f7ed81e
Jhonatan Wendel <wendeljhonatan@gmail.com>
2022-11-09 02:03:04 -0300

    electronics shop [PY]

---
+++ HackerRank/electronics shop/solution.py
@@ -0,0 +1,9 @@
+def getMoneySpent(keyboards, drives, b):
+    cost = -1
+    
+    for i in keyboards:
+        for j in drives:
+            if((i + j) <= b and (i + j) > cost):
+                cost = i+j
+    
+    return cost
